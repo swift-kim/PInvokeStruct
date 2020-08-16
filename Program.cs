99ed69faa81f72eb3278bf7ddebb13ad6e72c436
Swift Kim <swift.kim@samsung.com>
2020-08-16 18:24:55 +0900

    Code clean-up

--- Program.cs
+++ Program.cs
@@ -18,89 +18,38 @@ namespace PInvokeStruct
         [DllImport("/mnt/d/Git/pinvoke-struct/PInvokeStruct/libnative.so")]
         internal static extern void NativeFoo([In] ref Properties properties);
 
-        static unsafe IntPtr ToNativeArray(IList<string> strings)
+        static unsafe IntPtr ToNativeArray(string[] strings)
         {
             var pointers = new List<IntPtr>();
 
             foreach (var s in strings)
             {
-                var bytes = Encoding.ASCII.GetBytes(s);
-                var pBytes = IntPtr.Zero;
-                fixed (byte* p = bytes)
+                fixed (byte* pb = Encoding.ASCII.GetBytes(s))
                 {
-                    pBytes = (IntPtr)p;
+                    pointers.Add((IntPtr)pb);
                 }
-                pointers.Add(pBytes);
             }
 
-            fixed (IntPtr* p = pointers.ToArray())
+            fixed (IntPtr* pp = pointers.ToArray())
             {
-                return (IntPtr)p;
+                return (IntPtr)pp;
             }
         }
 
-        static unsafe void Main(string[] args)
+        static void Main(string[] args)
         {
-            var switches = new List<string>
+            var switches = new string[]
             {
                 "--verbose-logging",
                 "--trace-startup",
                 "--disable-service-auth-codes",
             };
 
-            //var pointers = new List<IntPtr>();
-
-            //foreach (var s in switches)
-            //{
-            //    var bytes = Encoding.ASCII.GetBytes(s);
-            //    var pBytes = IntPtr.Zero;
-            //    fixed (byte* p = bytes)
-            //    {
-            //        pBytes = (IntPtr)p;
-            //    }
-            //    pointers.Add(pBytes);
-            //}
-            //var ppBytes = IntPtr.Zero;
-            //fixed (IntPtr* p = pointers.ToArray())
-            //{
-            //    ppBytes = (IntPtr)p;
-            //}
-
-            //var bytes = Encoding.ASCII.GetBytes(switches[0]);
-
-            //// Method 3
-            //IntPtr pBytes = IntPtr.Zero;
-            //fixed (byte* p = bytes)
-            //{
-            //    pBytes = (IntPtr)p;
-            //}
-
-            //var b = new IntPtr[] { pBytes };
-            //IntPtr ppBytes = IntPtr.Zero;
-            //fixed (IntPtr* p = b)
-            //{
-            //    ppBytes = (IntPtr)p;
-            //}
-
-            // Method 2
-            //GCHandle pinnedArray = GCHandle.Alloc(bytes, GCHandleType.Pinned);
-            //IntPtr buffer = pinnedArray.AddrOfPinnedObject();
-
-            // Method 1 (works?)
-            //var buffer = Marshal.AllocHGlobal(bytes.Length);
-            //Marshal.Copy(bytes, 0, buffer, bytes.Length);
-
-            //ReadOnlySpan<byte> span = new ReadOnlySpan<byte>(buffer.ToPointer(), 4);
-            //var reference = MemoryMarshal.GetReference(span);
-
             var properties = new Properties
             {
                 switches = ToNativeArray(switches),
-                switches_count = (uint)switches.Count
+                switches_count = (uint)switches.Length
             };
-
-            //pinnedArray.Free();
-            //Marshal.FreeHGlobal(buffer);
 
             NativeFoo(ref Unsafe.AsRef(properties));
         }
